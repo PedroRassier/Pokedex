f1f3fb4580641093d1598c64bc047cd42d0991e9
Pedro Rassier <pedrorassier@gmail.com>
2022-11-29 19:09:47 -0300

    feature: fetch success validation

--- src/contexts/SearchContext.jsx
+++ src/contexts/SearchContext.jsx
@@ -7,23 +7,36 @@ export const SearchContext = createContext();
 export default function SearchContextProvider({ children }) {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchPokemon, setSearchPokemon] = useState(null);
+  const [fetchSuccess, setFetchSuccess] = useState(true);
 
   useEffect(() => {
     if (searchTerm) {
       fetch(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`)
         .then((response) => response.json())
-        .then((data) => setSearchPokemon(data));
+        .then((data) => {
+          setSearchPokemon(data);
+          setFetchSuccess(true);
+        })
+        .catch(() => setFetchSuccess(false));
       console.log(searchPokemon);
     }
   }, [searchTerm]);
 
   const handleChangeOnSearchTerm = (data) => {
+    if (searchTerm === "") {
+      setSearchPokemon(null);
+    }
     setSearchTerm(data);
   };
 
   return (
     <SearchContext.Provider
-      value={{ searchTerm, handleChangeOnSearchTerm, searchPokemon }}
+      value={{
+        searchTerm,
+        handleChangeOnSearchTerm,
+        searchPokemon,
+        fetchSuccess,
+      }}
     >
       {children}
     </SearchContext.Provider>
